8902c68d0e7359d0f8609fc90da767f2c2297fcc
Ashique01 <ashiquemura@gmail.com>
2024-08-24 19:47:24 +0600

    full project committed

---
+++ Vangti/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Vangti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTotalCost = new System.Windows.Forms.TextBox();
+            this.txtMoneyPresented = new System.Windows.Forms.TextBox();
+            this.btnVangti = new System.Windows.Forms.Button();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(331, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vangti";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(42, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Total Cost";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(174, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Money Presented";
+            // 
+            // txtTotalCost
+            // 
+            this.txtTotalCost.Location = new System.Drawing.Point(30, 140);
+            this.txtTotalCost.Name = "txtTotalCost";
+            this.txtTotalCost.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalCost.TabIndex = 3;
+            // 
+            // txtMoneyPresented
+            // 
+            this.txtMoneyPresented.Location = new System.Drawing.Point(189, 140);
+            this.txtMoneyPresented.Name = "txtMoneyPresented";
+            this.txtMoneyPresented.Size = new System.Drawing.Size(100, 20);
+            this.txtMoneyPresented.TabIndex = 3;
+            // 
+            // btnVangti
+            // 
+            this.btnVangti.BackColor = System.Drawing.Color.LightGreen;
+            this.btnVangti.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnVangti.Location = new System.Drawing.Point(43, 236);
+            this.btnVangti.Name = "btnVangti";
+            this.btnVangti.Padding = new System.Windows.Forms.Padding(5);
+            this.btnVangti.Size = new System.Drawing.Size(75, 35);
+            this.btnVangti.TabIndex = 4;
+            this.btnVangti.Text = "Changes";
+            this.btnVangti.UseVisualStyleBackColor = false;
+            this.btnVangti.Click += new System.EventHandler(this.btnVangti_Click);
+            // 
+            // lstBox
+            // 
+            this.lstBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(141, 190);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(168, 121);
+            this.lstBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 327);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.btnVangti);
+            this.Controls.Add(this.txtMoneyPresented);
+            this.Controls.Add(this.txtTotalCost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTotalCost;
+        private System.Windows.Forms.TextBox txtMoneyPresented;
+        private System.Windows.Forms.Button btnVangti;
+        private System.Windows.Forms.ListBox lstBox;
+    }
+}
+
---
+++ Vangti/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vangti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayVangti(decimal diff)
+        {
+            if (diff >= 1000)
+            {
+                lstBox.Items.Add(((int)diff / 1000).ToString() + " Thousand Taka Note(s)");
+                diff -= ((int)diff / 1000) * 1000;
+            }
+            if (diff >= 500)
+            {
+                lstBox.Items.Add(((int)diff / 500).ToString() + " 500 Taka Note(s)");
+                diff -= ((int)diff / 500) * 500;
+            }
+            if (diff >= 200)
+            {
+                lstBox.Items.Add(((int)diff / 200).ToString() + " 200 Taka Note(s)");
+                diff -= ((int)diff / 200) * 200;
+            }
+            if (diff >= 100)
+            {
+                lstBox.Items.Add(((int)diff / 100).ToString() + " 100 Taka Note(s)");
+                diff -= ((int)diff / 100) * 100;
+            }
+            if (diff >= 50)
+            {
+                lstBox.Items.Add(((int)diff / 50).ToString() + " 50 Taka Note(s)");
+                diff -= ((int)diff / 50) * 50;
+            }
+            if (diff >= 20)
+            {
+                lstBox.Items.Add(((int)diff / 20).ToString() + " 20 Taka Note(s)");
+                diff -= ((int)diff / 20) * 20;
+            }
+            if (diff >= 10)
+            {
+                lstBox.Items.Add(((int)diff / 10).ToString() + " 10 Taka Note(s)");
+                diff -= ((int)diff / 10) * 10;
+            }
+            if (diff >= 5)
+            {
+                lstBox.Items.Add(((int)diff / 5).ToString() + " 5 Taka Note(s)/Coin(s)");
+                diff -= ((int)diff / 5) * 5;
+            }
+            if (diff >= 2)
+            {
+                lstBox.Items.Add(((int)diff / 2m).ToString() + " 2 Taka Coin(s)");
+                diff -= ((int)diff / 2m) * 2m;
+            }
+            if (diff >= 1)
+            {
+                lstBox.Items.Add(((int)diff / 1m).ToString() + " 1 Taka Coin(s)");
+                diff -= ((int)diff / 1m) * 1m;
+            }
+
+            if (diff != 0)
+            {
+                lstBox.Items.Add(diff.ToString());  // Handle any remaining fractional amount, if needed.
+            }
+        }
+
+        private void btnVangti_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal cost = decimal.Parse(txtTotalCost.Text);
+                decimal moneyPresented = decimal.Parse(txtMoneyPresented.Text);
+
+                lstBox.Items.Clear();
+
+                if (cost > moneyPresented)
+                {
+                    MessageBox.Show("You need more money", "Vangti");
+                }
+
+                else
+                {
+                    decimal diff = Math.Abs(moneyPresented - cost);
+                    DisplayVangti(diff);
+
+                }
+            }
+            catch
+            {
+                txtTotalCost.Text = "";
+                txtMoneyPresented.Text = "";
+                txtTotalCost.Focus();
+            }
+
+        }
+    }
+}
